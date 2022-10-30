21505bceccbb936eb7439aa9e7692bacf8498430
Zak Salazar <therangerzakshow@Thes-MacBook-Pro.local>
2022-10-29 21:15:37 -0700

    changed keys to reflect hours

--- script.js
+++ script.js
@@ -15,10 +15,12 @@ document.getElementById("save9").onclick = function () {
   var time = document.getElementsByClassName("time9");
   var textArea = document.getElementsByClassName("task9");
 
+  var timeresult = time[0].textContent;
   var results = textArea[0].value; 
 
   console.log(results);
-  localStorage.setItem('task', JSON.stringify(results)); 
+  console.log(timeresult + results);
+  localStorage.setItem('9AM', JSON.stringify(results)); 
 };
 document.getElementById("save10").onclick = function () {
   var time = document.getElementsByClassName("time10");
@@ -27,7 +29,7 @@ document.getElementById("save10").onclick = function () {
   var results = textArea[0].value; 
 
   console.log(results);
-  localStorage.setItem('task', JSON.stringify(results)); 
+  localStorage.setItem('10AM', JSON.stringify(results)); 
 };
 document.getElementById("save11").onclick = function () {
   var time = document.getElementsByClassName("time11");
@@ -35,7 +37,7 @@ document.getElementById("save11").onclick = function () {
 
   var results = textArea[0].value; 
   console.log(results);
-  localStorage.setItem('task', JSON.stringify(results)); 
+  localStorage.setItem('11AM', JSON.stringify(results)); 
 };
 document.getElementById("save12").onclick = function () {
   var time = document.getElementsByClassName("time12");
@@ -43,7 +45,7 @@ document.getElementById("save12").onclick = function () {
 
   var results = textArea[0].value; 
   console.log(results);
-  localStorage.setItem('task', JSON.stringify(results)); 
+  localStorage.setItem('12PM', JSON.stringify(results)); 
 };
 document.getElementById("save13").onclick = function () {
   var time = document.getElementsByClassName("time13");
@@ -51,7 +53,7 @@ document.getElementById("save13").onclick = function () {
 
   var results = textArea[0].value; 
   console.log(results);
-  localStorage.setItem('task', JSON.stringify(results)); 
+  localStorage.setItem('1PM', JSON.stringify(results)); 
 };
 document.getElementById("save14").onclick = function () {
   var time = document.getElementsByClassName("time14");
@@ -59,7 +61,7 @@ document.getElementById("save14").onclick = function () {
 
   var results = textArea[0].value; 
   console.log(results);
-  localStorage.setItem('task', JSON.stringify(results)); 
+  localStorage.setItem('2PM', JSON.stringify(results)); 
 };
 document.getElementById("save15").onclick = function () {
   var time = document.getElementsByClassName("time15");
@@ -67,7 +69,7 @@ document.getElementById("save15").onclick = function () {
 
   var results = textArea[0].value; 
   console.log(results);
-  localStorage.setItem('task', JSON.stringify(results)); 
+  localStorage.setItem('3PM', JSON.stringify(results)); 
 };
 document.getElementById("save16").onclick = function () {
   var time = document.getElementsByClassName("time16");
@@ -75,7 +77,7 @@ document.getElementById("save16").onclick = function () {
 
   var results = textArea[0].value; 
   console.log(results);
-  localStorage.setItem('task', JSON.stringify(results)); 
+  localStorage.setItem('4PM', JSON.stringify(results)); 
 };
 document.getElementById("save17").onclick = function () {
   var time = document.getElementsByClassName("time17");
@@ -83,7 +85,7 @@ document.getElementById("save17").onclick = function () {
 
   var results = textArea[0].value; 
   console.log(results);
-  localStorage.setItem('task', JSON.stringify(results)); 
+  localStorage.setItem('5PM', JSON.stringify(results)); 
 };
 // each timeblock is color coded to indicate whether it is in the past, present, or future
 // if id <= current time background color: grey
