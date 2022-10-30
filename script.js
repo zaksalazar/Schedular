f6e511a61df7105d1f0d8a91589173c86f31e8a1
Zak Salazar <therangerzakshow@Thes-MacBook-Pro.local>
2022-10-30 08:05:30 -0700

    refactored vanilla js to jquery

--- script.js
+++ script.js
@@ -1,101 +1,55 @@
-var qs = function (tag) {
-  return document.getElementById(tag);
-};
-var currentTime = moment().hours();
-var blockTime = document.getElementsByClassName(".hour").value;
-var saveBtn = qs("saveBtn");
+$(document).ready(function () {
+  //the current day is displayed at the top of the calendar
+  $("#currentDay").text(
+    moment().format("dddd, MMMM Do, YYYY [and the time is] h:mm a")
+  );
 
-//the current day is displayed at the top of the calendar
+  //create event listener when saved button is clicked and save to local storage
+  $(".saveBtn").on("click", function () {
+    $('.saveBtn').css('background', '#77dd77');
+    var value = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
 
-$('#currentDay').text(moment().format('dddd, MMMM Do, YYYY [and the time is] h:mm a'));
+    // save in localStorage
+    localStorage.setItem(time, value);
+  });
+  // each timeblock is color coded to indicate whether it is in the past, present, or future
+  function updateTime() {
+    // get current number of hours
+    var currentHour = moment().hours();
 
-// create event listener when save button is clicked and save to local storage
-document.getElementById("save9").onclick = function () {
-  //localStorage.setItem(, tasks);
-  var time = document.getElementsByClassName("time9");
-  var textArea = document.getElementsByClassName("task9");
+    // loop over time blocks
+    $(".time-block").each(function () {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
 
-  var timeresult = time[0].textContent;
-  var results = textArea[0].value; 
+      // check if we've moved past this time
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
 
-  console.log(results);
-  console.log(timeresult + results);
-  localStorage.setItem('9AM', JSON.stringify(results)); 
-};
-document.getElementById("save10").onclick = function () {
-  var time = document.getElementsByClassName("time10");
-  var textArea = document.getElementsByClassName("task10");
+  updateTime();
 
-  var results = textArea[0].value; 
+  // set up interval to check if current time needs to be updated
+  var interval = setInterval(updateTime, 15000);
 
-  console.log(results);
-  localStorage.setItem('10AM', JSON.stringify(results)); 
-};
-document.getElementById("save11").onclick = function () {
-  var time = document.getElementsByClassName("time11");
-  var textArea = document.getElementsByClassName("task11");
-
-  var results = textArea[0].value; 
-  console.log(results);
-  localStorage.setItem('11AM', JSON.stringify(results)); 
-};
-document.getElementById("save12").onclick = function () {
-  var time = document.getElementsByClassName("time12");
-  var textArea = document.getElementsByClassName("task12");
-
-  var results = textArea[0].value; 
-  console.log(results);
-  localStorage.setItem('12PM', JSON.stringify(results)); 
-};
-document.getElementById("save13").onclick = function () {
-  var time = document.getElementsByClassName("time13");
-  var textArea = document.getElementsByClassName("task13");
-
-  var results = textArea[0].value; 
-  console.log(results);
-  localStorage.setItem('1PM', JSON.stringify(results)); 
-};
-document.getElementById("save14").onclick = function () {
-  var time = document.getElementsByClassName("time14");
-  var textArea = document.getElementsByClassName("task14");
-
-  var results = textArea[0].value; 
-  console.log(results);
-  localStorage.setItem('2PM', JSON.stringify(results)); 
-};
-document.getElementById("save15").onclick = function () {
-  var time = document.getElementsByClassName("time15");
-  var textArea = document.getElementsByClassName("task15");
-
-  var results = textArea[0].value; 
-  console.log(results);
-  localStorage.setItem('3PM', JSON.stringify(results)); 
-};
-document.getElementById("save16").onclick = function () {
-  var time = document.getElementsByClassName("time16");
-  var textArea = document.getElementsByClassName("task16");
-
-  var results = textArea[0].value; 
-  console.log(results);
-  localStorage.setItem('4PM', JSON.stringify(results)); 
-};
-document.getElementById("save17").onclick = function () {
-  var time = document.getElementsByClassName("time17");
-  var textArea = document.getElementsByClassName("task17");
-
-  var results = textArea[0].value; 
-  console.log(results);
-  localStorage.setItem('5PM', JSON.stringify(results)); 
-};
-// each timeblock is color coded to indicate whether it is in the past, present, or future
-// if id <= current time background color: grey
-if (blockTime < currentTime) {
-  blockTime.classList.add("past");
-} else if (blockTime === currentTime) {
-  blockTime.classList.remove("past");
-  blockTime.classList.add("present");
-} else if (blockTime > currentTime) {
-  blockTime.classList.remove("past");
-  blockTime.classList.remove("present");
-  blockTime.classList.add("future");
-}
+  // display schedule even after refresh, pull from localStorage
+  // load any saved data from localStorage
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+});
